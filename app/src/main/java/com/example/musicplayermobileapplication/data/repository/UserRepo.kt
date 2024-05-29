c92eba6f3bfeb74c8ae11f86ebd376a68f795b66
Bethanie Loh <143050470+TwinkleStar5@users.noreply.github.com>
2024-05-29 10:09:21 +0800

    UserRepo

---
+++ app/src/main/java/com/example/musicplayermobileapplication/data/repository/UserRepo.kt
@@ -0,0 +1,9 @@
+package com.example.musicplayermobileapplication.data.repository
+import com.example.musicplayermobileapplication.data.model.User
+
+interface UserRepo {
+    fun addUser(user: User)
+    fun getUser(username: String, password: Int): User?
+    fun updateUser(user: User)
+    fun deleteUser(user: User)
+}
